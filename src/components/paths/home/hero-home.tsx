7a9e6958b700df818eeb2d1f1c141cfebe9bdb18
David Adebayo <davidadebayo702@gmail.com>
2024-09-24 03:41:44 +0100

    zaza

--- src/components/paths/home/hero-home.tsx
+++ src/components/paths/home/hero-home.tsx
@@ -1,6 +1,5 @@
 import { ScrollRevealLeft, ScrollRevealRight } from "@/components/animations";
 import { RoundedBtnWithRightIcon } from "@/components/buttons";
-import { ReqDemoBtn, StartFreeTrailBtn } from "@/components/ui";
 import Image from "next/image";
 
 export const HeroHome = () => {
@@ -21,7 +20,7 @@ export const HeroHome = () => {
         className="absolute left-0 top-[50%] z-[10] h-auto w-[30%] translate-y-[-50%]"
         draggable={false}
       />
-      <div className="relative z-40 w-full pr-[5%] lg:max-w-[50%] lg:pr-0">
+      <div className="w-full pr-[5%] lg:max-w-[50%] lg:pr-0">
         <ScrollRevealLeft className="space-y-10 lg:space-y-[70px]">
           <div>
             <h2 className="text-center text-2xl font-bold text-[#297FB8] lg:text-left lg:text-[32px] lg:leading-tight">
@@ -32,8 +31,18 @@ export const HeroHome = () => {
             </h1>
           </div>
           <div className="flex flex-col items-center gap-6 lg:flex-row lg:gap-10">
-            <StartFreeTrailBtn />
-            <ReqDemoBtn />
+            <RoundedBtnWithRightIcon
+              btnText="Start Your Free Trial"
+              iconColor="#15181E"
+              className="w-full bg-[#FFFFFF] text-sm text-[#15181E] lg:w-fit"
+              style={{
+                boxShadow: "2.5px 2.5px 11.26px 0 rgba(255, 255, 255, 0.8)",
+              }}
+            />
+            <RoundedBtnWithRightIcon
+              btnText="Request a personalized demo"
+              className="w-full border border-solid border-white bg-transparent text-white/[0.98] lg:w-fit"
+            />
           </div>
         </ScrollRevealLeft>
       </div>
