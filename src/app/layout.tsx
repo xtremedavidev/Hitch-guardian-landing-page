3daaf06d874620c6a52f0b6187098a797629290b
David Adebayo <davidadebayo702@gmail.com>
2024-10-25 08:38:37 +0100

    fixed tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Import Script from next/script
 import "./globals.css";
 import { Footer, Navbar } from "@/components";
 import { GoogleTagManager } from "@next/third-parties/google";
@@ -21,11 +24,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
-        <GoogleTagManager
-          gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}
+      <head>
+        {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
+          <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
+        )}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16742497543"
         />
-      )}
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16742497543');
+          `}
+        </Script>
+        <Script id="conversion-event">
+          {`
+            gtag('event', 'conversion', {'send_to': 'AW-16742497543/ebxfCMzX8d0ZEIf6uK8-'});
+          `}
+        </Script>
+      </head>
+
       <body className={`${inter.className} bg-[#010003] text-white`}>
         <Navbar />
         {children}
